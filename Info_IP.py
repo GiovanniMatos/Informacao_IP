8e99546bee013c33a27041af77002b319c09a420
GiovanniMatos <giovannyafonso23@gmail.com>
2023-09-14 09:38:44 -0300

    commit

--- Info_IP.py
+++ Info_IP.py
@@ -18,34 +18,37 @@ marcador = input("Nome da sessão (opcional): ")
 
 request = requests.get(api + ip).json()
 
+def info():
 
-print(f"""
-\033[36m [+]Endereco IP:\033[0;0m              {request['ip']}
-\033[36m Ativo:\033[0;0m                       {request['success']} 
-\033[36m Tipo:\033[0;0m                        {request['type']}   
-\033[36m Continente:\033[0;0m                  {request['continent']}
-\033[36m Codigo do Continente:\033[0;0m        {request['continent_code']}
-\033[36m País:\033[0;0m                        {request['country']}
-\033[36m Codigo do País:\033[0;0m              {request['country_code']}
-\033[36m Bandeira:\033[0;0m                    {request['country_flag']}
-\033[36m Capital:\033[0;0m                     {request['country_capital']}
-\033[36m DDD:\033[0;0m                         {request['country_phone']}
-\033[36m Países Vizinhos:\033[0;0m             {request['country_neighbours']}
-\033[36m Cidade:\033[0;0m                      {request['city']}
-\033[36m Latitude:\033[0;0m                    {request['latitude']}
-\033[36m Longitude:\033[0;0m                   {request['longitude']}
-\033[36m ASN:\033[0;0m                         {request['asn']}
-\033[36m ORG:\033[0;0m                         {request['org']}
-\033[36m ISP:\033[0;0m                         {request['isp']}
-\033[36m Fuso Horario:\033[0;0m                {request['timezone_name']}
-\033[36m Fuso Horario DST Offset:\033[0;0m     {request['timezone_dstOffset']}
-\033[36m Fuso Horario GMT Offset:\033[0;0m     {request['timezone_gmtOffset']}
-\033[36m F H GMT:\033[0;0m                     {request['timezone_gmt']}
-\033[36m Codigo da Moeda:\033[0;0m             {request['currency_code']}
-\033[36m Simbolo da Moeda:\033[0;0m            {request['currency_symbol']}
-\033[36m Taxa de Cambio:\033[0;0m              {request['currency_rates']}
-\033[36m Moeda:\033[0;0m                       {request['currency_plural']}""")
-
+  print(f"""
+  \033[36m [+]Endereco IP:\033[0;0m              {request['ip']}
+  \033[36m Ativo:\033[0;0m                       {request['success']} 
+  \033[36m Tipo:\033[0;0m                        {request['type']}   
+  \033[36m Continente:\033[0;0m                  {request['continent']}
+  \033[36m Codigo do Continente:\033[0;0m        {request['continent_code']}
+  \033[36m País:\033[0;0m                        {request['country']}
+  \033[36m Codigo do País:\033[0;0m              {request['country_code']}
+  \033[36m Bandeira:\033[0;0m                    {request['country_flag']}
+  \033[36m Capital:\033[0;0m                     {request['country_capital']}
+  \033[36m DDD:\033[0;0m                         {request['country_phone']}
+  \033[36m Países Vizinhos:\033[0;0m             {request['country_neighbours']}
+  \033[36m Cidade:\033[0;0m                      {request['city']}
+  \033[36m Latitude:\033[0;0m                    {request['latitude']}
+  \033[36m Longitude:\033[0;0m                   {request['longitude']}
+  \033[36m ASN:\033[0;0m                         {request['asn']}
+  \033[36m ORG:\033[0;0m                         {request['org']}
+  \033[36m ISP:\033[0;0m                         {request['isp']}
+  \033[36m Fuso Horario:\033[0;0m                {request['timezone_name']}
+  \033[36m Fuso Horario DST Offset:\033[0;0m     {request['timezone_dstOffset']}
+  \033[36m Fuso Horario GMT Offset:\033[0;0m     {request['timezone_gmtOffset']}
+  \033[36m F H GMT:\033[0;0m                     {request['timezone_gmt']}
+  \033[36m Codigo da Moeda:\033[0;0m             {request['currency_code']}
+  \033[36m Simbolo da Moeda:\033[0;0m            {request['currency_symbol']}
+  \033[36m Taxa de Cambio:\033[0;0m              {request['currency_rates']}
+  \033[36m Moeda:\033[0;0m                       {request['currency_plural']}""")
+if __name__ == '__main__':
+  info()
+  
 latitude = request['latitude']
 longitude = request['longitude']
 mymap = folium.Map(location=[latitude, longitude], zoom_start=12)
